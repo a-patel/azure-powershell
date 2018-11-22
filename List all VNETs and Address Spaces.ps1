174a4abb9c859b593bb452e936ec576ad1ab780f
Aashish Patel <toaashishpatel@gmail.com>
2018-11-22 19:58:13 +0530

    Add files via upload

---
+++ List all VNETs and Address Spaces.ps1
@@ -0,0 +1,11 @@
+﻿$subs = Get-AzureRmSubscription 
+foreach ($Sub in $Subs) { 
+    #$SelectSub = Select-AzureRmSubscription -SubscriptionName $Sub.Name 
+    $VNETs = Get-AzureRmVirtualNetwork 
+    foreach ($VNET in $VNETs) { 
+        $Sub.Name 
+        $VNET.Name 
+        ($VNET).AddressSpace.AddressPrefixes 
+        Write-Host " " 
+    } 
+}
