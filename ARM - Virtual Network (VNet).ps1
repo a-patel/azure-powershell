fca8f873ce148eb464710cc294d26a786d252c09
Aashish Patel <toaashishpatel@gmail.com>
2018-11-26 19:02:50 +0530

    Update ARM - Virtual Network (VNet).ps1

--- ARM - Virtual Network (VNet).ps1
+++ ARM - Virtual Network (VNet).ps1
@@ -1,22 +1,8 @@
 
-
-# Variables for common values
-
-$tags = New-Object 'System.Collections.Generic.Dictionary[String,object]'
-$tags.Add("author", "Ashish")
-$tags.Add("project", "demo")
+<# Create Virtual Network, if it does not exist #>
 
 
-$location = "eastus2"
-
-
-# Create Virtual Network, if it doesn't already exist
-
-
-### https://docs.microsoft.com/en-us/azure/virtual-network/quick-create-powershell
-### https://docs.microsoft.com/en-us/powershell/module/azurerm.network/get-azurermvirtualnetwork?view=azurermps-6.13.0
-### https://github.com/robotechredmond/Azure-PowerShell-Snippets/blob/master/Azure%20Resource%20Manager%20-%20Create%20V2%20environment%20w%20VNET%20GW%20demo.ps1
-
+# Variables - Virtual Network
 
 $vnetShortName = "myvnet"
 $vnetSuffix = "-vent"
@@ -27,24 +13,32 @@ $vnetName = "${vnetShortName}${vnetSuffix}"
 Get-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $rgName -ErrorVariable isVNetExist -ErrorAction SilentlyContinue `
 
 
-
-If ($isVNetExist) {
-
-    # VNet doesn't exist
-    "VNet doesn't exist"
-
-
-    $subnet1Name = "subnet01"
-    $subnet2Name = "GatewaySubnet"
+If ($isVNetExist) 
+{
+    Write-Output "Virtual Network does not exist"
     
+
+
+    $subnet1Name = "web"
+    
+    Write-Verbose "Creating new subnet: {$subnet1Name}"
+
     $subnet1 = New-AzureRmVirtualNetworkSubnetConfig `
         -Name $subnet1Name `
         -AddressPrefix "10.0.1.0/24"
+
+
+    $subnet2Name = "GatewaySubnet"
+
+    Write-Verbose "Creating new subnet: {$subnet2Name}"
 
     $subnet2 = New-AzureRmVirtualNetworkSubnetConfig `
         -Name $subnet2Name `
         -AddressPrefix "10.0.2.0/28"
 
+
+
+    Write-Verbose "Creating new Virtual Network: {$vnetName}"
 
     $vnet = New-AzureRmVirtualNetwork `
               -Name $vnetName `
@@ -53,11 +47,13 @@ If ($isVNetExist) {
               -AddressPrefix 10.0.0.0/16 `
               -Subnet $subnet1, $subnet2 `
               -Tag $tags
+}
+Else 
+{
+    Write-Output "Virtual Network exist"
 
-} Else {
+    Write-Verbose "Fetching Virtual Network: {$vnetName}"
 
-    # VNet exist
-    "VNet exist"
 
     $vnet = Get-AzureRmVirtualNetwork `
             -Name $vnetName `
@@ -66,7 +62,9 @@ If ($isVNetExist) {
 
 
 
-# Get list of all VNets
+Write-Verbose Get list of all VNets
+Write-Output "Virtual Networks"
+
 
 Get-AzureRmVirtualNetwork -ResourceGroupName $rgName `
     | Select-Object Name, ResourceGroupName, Location `
@@ -88,7 +86,9 @@ Get-AzureRmVirtualNetwork `
 
 <#
 
-
+https://docs.microsoft.com/en-us/azure/virtual-network/quick-create-powershell
+https://docs.microsoft.com/en-us/powershell/module/azurerm.network/get-azurermvirtualnetwork?view=azurermps-6.13.0
+https://github.com/robotechredmond/Azure-PowerShell-Snippets/blob/master/Azure%20Resource%20Manager%20-%20Create%20V2%20environment%20w%20VNET%20GW%20demo.ps1
 
 #>
 
