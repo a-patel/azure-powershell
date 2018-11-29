2a730dc27d2dc220536615d7673079aaebe89446
Aashish Patel <toaashishpatel@gmail.com>
2018-11-29 15:48:39 +0530

    Update ARM - Network Interface (NIC).ps1

--- ARM - Network Interface (NIC).ps1
+++ ARM - Network Interface (NIC).ps1
@@ -1,7 +1,85 @@
 
 
+## To Set Verbose output
+$PSDefaultParameterValues['*:Verbose'] = $true
+
+
+
+# Variables - Common
+
+$location = "eastus2"
+
+
+$tags = New-Object 'System.Collections.Generic.Dictionary[String,object]'
+$tags.Add("environment", "Production")         # Production, Staging, QA
+$tags.Add("projectName", "Demo Project")
+$tags.Add("projectVersion", "1.0.0")
+$tags.Add("managedBy", "developer.aashishpatel@gmail.com")
+$tags.Add("billTo", "Ashish Patel")
+$tags.Add("tier", "Front End")                 # Front End, Back End, Data
+$tags.Add("dataProfile", "Public")             # Public, Confidential, Restricted, Internal
+
+
+
+
+
+<# Resource Group #>
+
+
+# Variables - Resource Group
+
+$rgShortName = "qweasdzxc"
+$rgSuffix = "-rg"
+$rgName = "${rgShortName}${rgSuffix}"
+
+
+<# Virtual Network (VNet) #>
+
+<#
+
+
+#>
+
+
+# Variables - Virtual Network
+
+$vnetShortName = "qweasdzxc"
+$vnetSuffix = "-vnet"
+$vnetName = "${vnetShortName}${vnetSuffix}"
+
+# subnets
+$webSubnetName = "WebSubnet"
+$frontendSubnetName = "FrontEndSubnet"
+$backendSubnetName = "BackEndSubnet"
+$gatewaySubnetName = "GatewaySubnet"
+
+# Variables - Public IP
+
+$publicIpShortName = "qweasdzxc"
+$publicIpSuffix = "-ip"
+$publicIpName = "${publicIpShortName}${publicIpSuffix}"
+$dnsPrefix  = "qweasdzxc"
+#$dnsPrefix  = "qweasdzxc$(Get-Random)"
+
+# Variables - Network Security Group
+
+$nsgShortName = "qweasdzxc"
+$nsgSuffix = "-nsg"
+$nsgName = "${nsgShortName}${nsgSuffix}"
+
+
+
 
 <# Network Interface (NIC) #>
+
+<#
+
+Virtual Network (VNet)
+Subnet
+Public IP address
+Network Security Group (NSG) 
+
+#>
 
 
 # Variables - Network Interface (NIC)
@@ -10,6 +88,7 @@ $nicShortName = "qweasdzxc"
 $nicSuffix = "-nic"
 $nicName = "${nicShortName}${nicSuffix}"
 
+$subnetName = "WebSubnet"
 
 
 <# Create Network Interface (NIC), if it does not exist #>
@@ -22,21 +101,26 @@ If ($isNICExist)
     Write-Output "Network Interface (NIC) does not exist"
 
     
-    # virtual network
-    Write-Verbose "Fetching Virtual Network: {$vnetName}"
+    # Virtual Network (VNet)
+    Write-Verbose "Fetching Virtual Network (VNet): {$vnetName}"
     
     $vnet = Get-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $rgName
 
-    ## TODO: Get subnet by name
 
-    # public IP address 
+    # Subnet
+    Write-Verbose "Fetching Subnet: {$subnetName}"
+
+    $subnet = Get-AzureRmVirtualNetworkSubnetConfig -Name $subnetName -VirtualNetwork $vnet
+
+
+    # Public IP address 
     Write-Verbose "Fetching Public IP: {$publicIpName}"
 
     $publicIp = Get-AzureRmPublicIpAddress -Name $publicIpName -ResourceGroupName $rgName
 
 
-    # network security group 
-    Write-Verbose "Fetching Network Security Group: {$nsgName}"
+    # Network Security Group (NSG) 
+    Write-Verbose "Fetching Network Security Group (NSG): {$nsgName}"
 
     $nsg = Get-AzureRmNetworkSecurityGroup -Name $nsgName -ResourceGroupName $rgName
 
@@ -49,7 +133,7 @@ If ($isNICExist)
                 -Name $nicName `
                 -ResourceGroupName $rgName `
                 -Location $location `
-                -SubnetId $vnet.Subnets[0].Id `
+                -SubnetId $subnet.Id `
                 -PublicIpAddressId $pip.Id `
                 -NetworkSecurityGroupId $nsg.Id
 } 
