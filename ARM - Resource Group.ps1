54d2846c31ae3f0c863377b25a9cb89dd0aad111
Aashish Patel <toaashishpatel@gmail.com>
2018-11-29 13:18:50 +0530

    Update ARM - Resource Group.ps1

--- ARM - Resource Group.ps1
+++ ARM - Resource Group.ps1
@@ -42,8 +42,8 @@ If ($isRGExist)
 {
     Write-Output "Resource Group does not exist"
     
-    Write-Verbose "Creating new Resource Group: {$rgName}"
 
+    Write-Verbose "Creating new Resource Group: {$rgName}"
 
     $rg = New-AzureRmResourceGroup `
             -Name $rgName `
@@ -54,11 +54,10 @@ Else
 {
     Write-Output "Resource Group exist"
 
+
     Write-Verbose "Fetching Resource Group: {$rgName}"
 
-
-    $rg = Get-AzureRmResourceGroup `
-            -Name $rgName 
+    $rg = Get-AzureRmResourceGroup -Name $rgName 
 }
 
 
@@ -67,11 +66,14 @@ Write-Verbose "Get list of Resource Groups"
 Write-Output "Resource Groups"
 
 
-Get-AzureRmResourceGroup | Select-Object ResourceGroupName, Location `
-                         | Format-Table -AutoSize -Wrap -GroupBy Location
+Get-AzureRmResourceGroup `
+    | Select-Object ResourceGroupName, Location `
+    | Format-Table -AutoSize -Wrap -GroupBy Location
 
 
 <#
+
+## Remove Resource Group
 
 $rgShortName = "aabbccddff"
 $rgSuffix = "-rg"
@@ -88,7 +90,9 @@ $jobRGDelete
 
 
 
+
 <#
+## References
 
 https://docs.microsoft.com/en-us/powershell/module/azurerm.resources/new-azurermresourcegroup?view=azurermps-6.13.0
 https://docs.microsoft.com/en-us/powershell/module/azurerm.resources/get-azurermresourcegroup?view=azurermps-6.13.0
