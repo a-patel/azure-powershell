51228fe95d8528156755e07921c0d86d5e5f148f
Aashish Patel <toaashishpatel@gmail.com>
2018-12-04 21:26:09 +0530

    Update ARM - Network Security Group (NSG) - Remove Security Rule.ps1

--- ARM - Network Security Group (NSG) - Remove Security Rule.ps1
+++ ARM - Network Security Group (NSG) - Remove Security Rule.ps1
@@ -1,10 +1,8 @@
-﻿
-<# Network Security Group (NSG) #>
+# Variables - Resource Group
 
-<#
-
-
-#>
+$rgShortName = "qweasdzxc"
+$rgSuffix = "-rg"
+$rgName = "${rgShortName}${rgSuffix}"
 
 
 # Variables - Network Security Group (NSG)
@@ -14,141 +12,49 @@ $nsgSuffix = "-nsg"
 $nsgName = "${nsgShortName}${nsgSuffix}"
 
 
-<# Create Network Security Group (NSG), if it does not exist - Rules below are example placeholders that allow selected traffic from all sources #>
+
+# Variables - Security Rule (NSG) 
+$ruleName = "HTTP"
+
+
+<# Network Security Group (NSG) - Remove Security Rule #>
+
+<#
+
+Network Security Group (NSG)
+Security Rule
+
+#>
+
 
 Get-AzureRmNetworkSecurityGroup -Name $nsgName -ResourceGroupName $rgName -ErrorVariable isNSGExist -ErrorAction SilentlyContinue `
 
 
-If ($isNSGExist) 
+If (!$isNSGExist) 
 {
-    Write-Output "Network Security Group does not exist"
+    Write-Output "Network Security Group exist"
     
 
-
-    Write-Verbose "Creating network security rule to Allow Inbound HTTP (Port: 80): {HTTP}"
-
-    $nsgRuleHTTP = New-AzureRmNetworkSecurityRuleConfig `
-        -Name "HTTP" `
-        -Description "Allow Inbound HTTP" `
-        -DestinationPortRange 80 `
-        -Priority 100 `
-        -Access Allow `
-        -Direction Inbound `
-        -Protocol Tcp `
-        -SourceAddressPrefix * `
-        -DestinationAddressPrefix * `
-        -SourcePortRange * `
-
-    
-    Write-Verbose "Creating network security rule to Allow Inbound HTTPS (Port: 443): {HTTPS}"
-
-    $nsgRuleHTTPS = New-AzureRmNetworkSecurityRuleConfig `
-        -Name "HTTPS" `
-        -Description "Allow Inbound HTTPS" `
-        -DestinationPortRange 443 `
-        -Priority 110 `
-        -Access Allow `
-        -Direction Inbound `
-        -Protocol Tcp `
-        -SourceAddressPrefix * `
-        -DestinationAddressPrefix * `
-        -SourcePortRange * `
-
-
-    Write-Verbose "Creating network security rule to Allow Inbound RDP (Port: 3389): {RDP}"
-
-    $nsgRuleRDP = New-AzureRmNetworkSecurityRuleConfig `
-        -Name "RDP" `
-        -Description "Allow Inbound RDP" `
-        -DestinationPortRange 3389 `
-        -Priority 400 `
-        -Access Allow `
-        -Direction Inbound `
-        -Protocol Tcp `
-        -SourceAddressPrefix * `
-        -DestinationAddressPrefix * `
-        -SourcePortRange * `
-
-
-    Write-Verbose "Creating network security rule to Allow Inbound SSH (Port: 22): {SSH}"
-
-    $nsgRuleSSH = New-AzureRmNetworkSecurityRuleConfig `
-        -Name "SSH" `
-        -Description "Allow Inbound SSH" `
-        -DestinationPortRange 22 `
-        -Priority 500 `
-        -Access Allow `
-        -Direction Inbound `
-        -Protocol Tcp `
-        -SourceAddressPrefix * `
-        -DestinationAddressPrefix * `
-        -SourcePortRange * `
+    Write-Verbose "Fetching Network Security Group: {$nsgName}"
+    $nsg = Get-AzureRmNetworkSecurityGroup -Name $nsgName -ResourceGroupName $rgName
 
 
 
-    Write-Verbose "Creating Network Security Group: {$nsgName}"
+    Write-Verbose "Removing network security rule: {$ruleName} (Port: {$rulePort})"
 
-    $nsg = New-AzureRmNetworkSecurityGroup `
-            -Name $nsgName `
-            -ResourceGroupName $rgName `
-            -Location $location `
-            -SecurityRules $nsgRuleHTTP, $nsgRuleHTTPS, $nsgRuleRDP, $nsgRuleSSH `
-            -Tag $tags
+    Remove-AzureRmNetworkSecurityRuleConfig -Name $ruleName -NetworkSecurityGroup $nsg
 } 
 Else 
 {
-    Write-Output "Network Security Group exist"
-
-
-    Write-Verbose "Fetching Network Security Group: {$nsgName}"
-
-    $nsg = Get-AzureRmNetworkSecurityGroup -Name $nsgName -ResourceGroupName $rgName
+    Write-Output "Network Security Group does not exist"
 }
-
-
-
-
-Write-Verbose "Get list of Network Security Group (NSG)"
-Write-Output "Network Security Groups"
-
-
-Get-AzureRmNetworkSecurityGroup -ResourceGroupName $rgName `
-    | Select-Object Name, ResourceGroupName, Location `
-    | Format-Table -AutoSize -Wrap
-
-
-<#
-
-Get-AzureRmNetworkSecurityGroup `
-    | Select-Object Name, ResourceGroupName, Location `
-    | Format-Table -AutoSize -Wrap -GroupBy ResourceGroupName
-
-#>
-
-
-
-<#
-
-## Remove Network Security Group (NSG)
-
-$nsgShortName = "qweasdzxc"
-$nsgSuffix = "-nsg"
-$nsgName = "${nsgShortName}${nsgSuffix}"
-
-
-Write-Verbose "Deleting Network Security Group (NSG): {$nsgName}"
-
-Remove-AzureRmNetworkSecurityGroup -Name $nsgName -ResourceGroupName $rgName -Force
-
-#>
-
 
 
 
 <#
 ## References
 
-https://docs.microsoft.com/en-us/powershell/module/azurerm.network/get-azurermnetworksecuritygroup?view=azurermps-6.13.0
-https://docs.microsoft.com/en-us/powershell/module/azurerm.network/get-azurermnetworksecurityruleconfig?view=azurermps-6.13.0&viewFallbackFrom=azurermps-6.12.0
+https://docs.microsoft.com/en-us/azure/virtual-network/manage-network-security-group
+https://docs.microsoft.com/en-us/powershell/module/azurerm.network/remove-azurermnetworksecurityruleconfig?view=azurermps-6.13.0
 
 #>
