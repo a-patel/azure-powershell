0e50ce6c059103d65ae2e6c5cbfe6082046559e8
Aashish Patel <toaashishpatel@gmail.com>
2018-12-04 21:24:40 +0530

    Update ARM - Network Interface (NIC) - Assign Public IP.ps1

--- ARM - Network Interface (NIC) - Assign Public IP.ps1
+++ ARM - Network Interface (NIC) - Assign Public IP.ps1
@@ -1,29 +1,9 @@
 
 
+
 ## To Set Verbose output
 $PSDefaultParameterValues['*:Verbose'] = $true
 
-
-
-# Variables - Common
-
-$location = "eastus2"
-
-
-$tags = New-Object 'System.Collections.Generic.Dictionary[String,object]'
-$tags.Add("environment", "Production")         # Production, Staging, QA
-$tags.Add("projectName", "Demo Project")
-$tags.Add("projectVersion", "1.0.0")
-$tags.Add("managedBy", "developer.aashishpatel@gmail.com")
-$tags.Add("billTo", "Ashish Patel")
-$tags.Add("tier", "Front End")                 # Front End, Back End, Data
-$tags.Add("dataProfile", "Public")             # Public, Confidential, Restricted, Internal
-
-
-
-
-
-<# Resource Group #>
 
 
 # Variables - Resource Group
@@ -33,53 +13,6 @@ $rgSuffix = "-rg"
 $rgName = "${rgShortName}${rgSuffix}"
 
 
-<# Virtual Network (VNet) #>
-
-<#
-
-
-#>
-
-
-# Variables - Virtual Network
-
-$vnetShortName = "qweasdzxc"
-$vnetSuffix = "-vnet"
-$vnetName = "${vnetShortName}${vnetSuffix}"
-
-# subnets
-$webSubnetName = "WebSubnet"
-$frontendSubnetName = "FrontEndSubnet"
-$backendSubnetName = "BackEndSubnet"
-$gatewaySubnetName = "GatewaySubnet"
-
-# Variables - Public IP
-
-$publicIpShortName = "qweasdzxc"
-$publicIpSuffix = "-ip"
-$publicIpName = "${publicIpShortName}${publicIpSuffix}"
-$dnsPrefix  = "qweasdzxc"
-#$dnsPrefix  = "qweasdzxc$(Get-Random)"
-
-# Variables - Network Security Group
-
-$nsgShortName = "qweasdzxc"
-$nsgSuffix = "-nsg"
-$nsgName = "${nsgShortName}${nsgSuffix}"
-
-
-
-
-<# Network Interface (NIC) #>
-
-<#
-
-Virtual Network (VNet)
-Subnet
-Public IP address
-Network Security Group (NSG) 
-
-#>
 
 
 # Variables - Network Interface (NIC)
@@ -88,113 +21,78 @@ $nicShortName = "qweasdzxc"
 $nicSuffix = "-nic"
 $nicName = "${nicShortName}${nicSuffix}"
 
-$subnetName = "WebSubnet"
+
+# Variables - Public IP
+
+$publicIpShortName = "qweasdzxc3"
+$publicIpSuffix = "-ip"
+$publicIpName = "${publicIpShortName}${publicIpSuffix}"
 
 
-<# Create Network Interface (NIC), if it does not exist #>
+
+# NOTE: NOT WORKING
+
+<# Network Interface (NIC) - Remove Public IP #>
+# NOTE: It only assign Public IP Address to NIC's IP Config
+
+<#
+
+Network Interface (NIC)
+Public IP address
+
+#>
+
+
 
 Get-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $rgName -ErrorVariable isNICExist -ErrorAction SilentlyContinue `
 
 
-If ($isNICExist) 
-{
-    Write-Output "Network Interface (NIC) does not exist"
-
-    
-    # Virtual Network (VNet)
-    Write-Verbose "Fetching Virtual Network (VNet): {$vnetName}"
-    
-    $vnet = Get-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $rgName
-
-
-    # Subnet
-    Write-Verbose "Fetching Subnet: {$subnetName}"
-
-    $subnet = Get-AzureRmVirtualNetworkSubnetConfig -Name $subnetName -VirtualNetwork $vnet
-
-
-    # Public IP address 
-    Write-Verbose "Fetching Public IP: {$publicIpName}"
-
-    $publicIp = Get-AzureRmPublicIpAddress -Name $publicIpName -ResourceGroupName $rgName
-
-
-    # Network Security Group (NSG) 
-    Write-Verbose "Fetching Network Security Group (NSG): {$nsgName}"
-
-    $nsg = Get-AzureRmNetworkSecurityGroup -Name $nsgName -ResourceGroupName $rgName
-
-
-
-    # Create a virtual network card and associate with public IP address and NSG
-    Write-Verbose "Creating Network Interface (NIC): {$nicName}"
-
-    $nic = New-AzureRmNetworkInterface `
-                -Name $nicName `
-                -ResourceGroupName $rgName `
-                -Location $location `
-                -Subnet $subnet `
-                -PublicIpAddress $publicIp `
-                -NetworkSecurityGroup $nsg `
-                -Tag $tags 
-} 
-Else 
+If (!$isNICExist) 
 {
     Write-Output "Network Interface (NIC) exist"
 
 
-    Write-Verbose "Fetching Network Interface (NIC): {$rgName}"
-
+    Write-Verbose "Fetching Network Interface (NIC): {$nicName}"
     $nic = Get-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $rgName
+
+    
+    Write-Verbose "Fetching Public IP: {$publicIpName}"
+    $publicIp = Get-AzureRmPublicIpAddress -Name $publicIpName -ResourceGroupName $rgName
+
+
+
+    Write-Verbose "Adding Public IP: {$publicIpName}  to Network Interface (NIC): {$nicName}"
+
+
+    $nic.IpConfigurations[0].PublicIpAddress = $publicIp 
+    Set-AzureRmNetworkInterface -NetworkInterface $nic
+
+
+<#
+$asg = New-AzureRmApplicationSecurityGroup `
+    -Name MyASG `
+    -ResourceGroupName $rgName `
+    -Location $location `
+
+$nic | Set-AzureRmNetworkInterfaceIpConfig -Name $nic.IpConfigurations[0].Name -Subnet $vnet.Subnets[0] -ApplicationSecurityGroup $asg | Set-AzureRmNetworkInterface
+
+$nic | Add-AzureRmNetworkInterfaceIpConfig -Name MyNewIpConfig -Subnet $vnet.Subnets[0] -ApplicationSecurityGroup $asg  | Set-AzureRmNetworkInterface
+#>
+} 
+Else 
+{
+    Write-Output "Network Interface (NIC) does not exist"
 }
 
 
-
-
-Write-Verbose "Get list of Network Interface (NIC)s"
-Write-Output "Network Interface (NIC)s"
-
-
-Get-AzureRmNetworkInterface -ResourceGroupName $rgName `
-    | Select-Object Name, ResourceGroupName, Location `
-    | Format-Table -AutoSize -Wrap
-
-
-<#
-
-Get-AzureRmNetworkInterface `
-    | Select-Object Name, ResourceGroupName, Location `
-    | Format-Table -AutoSize -Wrap -GroupBy ResourceGroupName
-
-#>
-
-
-
-
-<#
-
-## Remove Network Interface (NIC)
-
-$nicShortName = "qweasdzxc"
-$nicSuffix = "-nic"
-$nicName = "${nicShortName}${nicSuffix}"
-
-
-Write-Verbose "Delete Network Interface (NIC): {$nicName}"
-
-Remove-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $rgName -Force
-
-#>
 
 
 
 <#
 ## References
 
-
+https://stackoverflow.com/questions/34164282/assigning-a-public-ip-address-to-an-existing-nic-in-azure-using-powershell
+https://docs.microsoft.com/en-us/powershell/module/azurerm.network/add-azurermnetworkinterfaceipconfig?view=azurermps-6.13.0
 
 #>
-
-
-
 
