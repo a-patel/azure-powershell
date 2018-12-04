ba936f92fa62a8fec40cb192f31a48a03421f8c0
Aashish Patel <toaashishpatel@gmail.com>
2018-12-04 21:29:40 +0530

    Update ARM - Virtual Network (VNet) - Remove Subnet.ps1

--- ARM - Virtual Network (VNet) - Remove Subnet.ps1
+++ ARM - Virtual Network (VNet) - Remove Subnet.ps1
@@ -1,5 +1,19 @@
-﻿
-<# Subnet #>
+# Variables - Resource Group
+
+$rgShortName = "qweasdzxc"
+$rgSuffix = "-rg"
+$rgName = "${rgShortName}${rgSuffix}"
+
+# Variables - Virtual Network
+
+$vnetShortName = "qweasdzxc"
+$vnetSuffix = "-vnet"
+$vnetName = "${vnetShortName}${vnetSuffix}"
+
+
+
+
+<# Remove Subnet - from Virtual Network (VNet) #>
 
 <#
 Virtual Network (VNet)
@@ -18,36 +32,12 @@ $addressPrefix = "10.0.6.0/24"
 
 <# Create Subnet (in VNet), if it does not exist #>
 
-$vnet = Get-AzureRmVirtualNetwork `
-        -Name $vnetName `
-        -ResourceGroupName $rgName
+$vnet = Get-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $rgName
 
 Get-AzureRmVirtualNetworkSubnetConfig -Name $subnetName -VirtualNetwork $vnet -ErrorVariable isSubnetExist -ErrorAction SilentlyContinue `
 
 
-If ($isSubnetExist) 
-{
-    Write-Output "Subnet does not exist"
-
-
-    Write-Verbose "Fetching Virtual Network: {$vnetName}"
-
-    $vnet = Get-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $rgName
-
-
-    # add a subnet to the in-memory representation of the virtual network. 
-    Write-Verbose "Creating Subnet: {$subnetName}"
-
-    Add-AzureRmVirtualNetworkSubnetConfig ` 
-        -Name $subnetName `
-        -VirtualNetwork $vnet `
-        -AddressPrefix $addressPrefix
-
-
-    # updates the existing virtual network with the new subnet.
-    $vnet | Set-AzureRmVirtualNetwork
-}
-Else 
+If (!$isSubnetExist) 
 {
     Write-Output "Subnet exist"
 
@@ -57,9 +47,18 @@ Else
     $vnet = Get-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $rgName
 
 
-    Write-Verbose "Fetching Subnet: {$subnetName}"
+    Write-Verbose "Removing Subnet: {$subnetName}"
 
-    $subnet = Get-AzureRmVirtualNetworkSubnetConfig -Name $subnetName -VirtualNetwork $vnet
+    Remove-AzureRmVirtualNetworkSubnetConfig -Name $subnetName -VirtualNetwork $vnet
+
+
+    # updates the existing virtual network with the new subnet.
+    $vnet | Set-AzureRmVirtualNetwork
+
+}
+Else 
+{
+    Write-Output "Subnet does not exist"
 }
 
 
@@ -80,35 +79,8 @@ Get-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vnet `
 
 
 <#
-
-## Remove Subnet from Virtual Network (VNet)
-
-$subnetShortName = "Test"
-$subnetSuffix = "subnet"
-$subnetName = "${subnetShortName}${subnetSuffix}"
-
-
-Write-Verbose "Fetching Virtual Network: {$vnetName}"
-
-$vnet = Get-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $rgName
-
-
-Write-Verbose "Removing Subnet: {$subnetName}"
-
-Remove-AzureRmVirtualNetworkSubnetConfig -Name $subnetName -VirtualNetwork $vnet
-
-
-# updates the existing virtual network with the new subnet.
-$vnet | Set-AzureRmVirtualNetwork
-
-#>
-
-
-<#
 ## References
 
-https://docs.microsoft.com/en-us/powershell/module/azurerm.network/add-azurermvirtualnetworksubnetconfig?view=azurermps-6.13.0
-https://docs.microsoft.com/en-us/powershell/module/azurerm.network/get-azurermvirtualnetworksubnetconfig?view=azurermps-6.13.0
 https://docs.microsoft.com/en-us/powershell/module/azurerm.network/remove-azurermvirtualnetworksubnetconfig?view=azurermps-6.13.0
 
 #>
