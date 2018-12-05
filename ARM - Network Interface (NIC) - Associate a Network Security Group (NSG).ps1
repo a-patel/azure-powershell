81c1ede5e86964a83d1c10440bff8958059f6aad
Aashish Patel <toaashishpatel@gmail.com>
2018-12-05 15:57:20 +0530

    Update ARM - Network Interface (NIC) - Associate a Network Security Group (NSG).ps1

--- ARM - Network Interface (NIC) - Associate a Network Security Group (NSG).ps1
+++ ARM - Network Interface (NIC) - Associate a Network Security Group (NSG).ps1
@@ -5,24 +5,6 @@ $PSDefaultParameterValues['*:Verbose'] = $true
 
 
 
-# Variables - Common
-
-$location = "eastus2"
-
-
-$tags = New-Object 'System.Collections.Generic.Dictionary[String,object]'
-$tags.Add("environment", "Production")         # Production, Staging, QA
-$tags.Add("projectName", "Demo Project")
-$tags.Add("projectVersion", "1.0.0")
-$tags.Add("managedBy", "developer.aashishpatel@gmail.com")
-$tags.Add("billTo", "Ashish Patel")
-$tags.Add("tier", "Front End")                 # Front End, Back End, Data
-$tags.Add("dataProfile", "Public")             # Public, Confidential, Restricted, Internal
-
-
-
-
-
 <# Resource Group #>
 
 
@@ -33,55 +15,11 @@ $rgSuffix = "-rg"
 $rgName = "${rgShortName}${rgSuffix}"
 
 
-<# Virtual Network (VNet) #>
-
-<#
-
-
-#>
-
-
-# Variables - Virtual Network
-
-$vnetShortName = "qweasdzxc"
-$vnetSuffix = "-vnet"
-$vnetName = "${vnetShortName}${vnetSuffix}"
-
-# subnets
-$webSubnetName = "WebSubnet"
-$frontendSubnetName = "FrontEndSubnet"
-$backendSubnetName = "BackEndSubnet"
-$gatewaySubnetName = "GatewaySubnet"
-
-# Variables - Public IP
-
-$publicIpShortName = "qweasdzxc3"
-$publicIpSuffix = "-ip"
-$publicIpName = "${publicIpShortName}${publicIpSuffix}"
-$dnsPrefix  = "qweasdzxc"
-#$dnsPrefix  = "qweasdzxc$(Get-Random)"
-
 # Variables - Network Security Group
 
 $nsgShortName = "qweasdzxc"
 $nsgSuffix = "-nsg"
 $nsgName = "${nsgShortName}${nsgSuffix}"
-
-
-
-$subnetName = "BackEndSubnet"
-
-
-<# Network Interface (NIC) #>
-
-<#
-
-Virtual Network (VNet)
-Subnet
-Network Security Group (NSG) (optional)
-Public IP address (optional)
-
-#>
 
 
 # Variables - Network Interface (NIC)
@@ -90,17 +28,19 @@ $nicShortName = "qweasdzxc2"
 $nicSuffix = "-nic"
 $nicName = "${nicShortName}${nicSuffix}"
 
-# NIC has public IP or not
-$hasPublicIp = $true
-
-# Static: Private Ip Address (within the subnet ip range)
-#$privateIpAddress = "10.0.2.77"
-
-# Dynamic: Private Ip Address
-$privateIpAddress = $null 
 
 
-<# Create Network Interface (NIC), if it does not exist #>
+<# Network Interface (NIC) - Associate/Dissociate a Network Security Group to a network interface #>
+
+<#
+
+Network Interface (NIC)
+Network Security Group (NSG)
+
+#>
+
+
+
 
 Get-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $rgName -ErrorVariable isNICExist -ErrorAction SilentlyContinue `
 
@@ -109,26 +49,21 @@ if (!$isNICExist)
 {
     Write-Output "Network Interface (NIC) exist"
 
-    
-    # Virtual Network (VNet)
-    Write-Verbose "Fetching Virtual Network (VNet): {$vnetName}"
-    $vnet = Get-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $rgName
-
-
-    # Subnet
-    Write-Verbose "Fetching Subnet: {$subnetName}"
-    $subnet = Get-AzureRmVirtualNetworkSubnetConfig -Name $subnetName -VirtualNetwork $vnet
-
 
     # Network Security Group (NSG) 
     Write-Verbose "Fetching Network Security Group (NSG): {$nsgName}"
     $nsg = Get-AzureRmNetworkSecurityGroup -Name $nsgName -ResourceGroupName $rgName
 
     
-
-    
-    Write-Verbose "Fetching Network Interface (NIC): {$rgName}"
+    # Network Interface (NIC) 
+    Write-Verbose "Fetching Network Interface (NIC): {$nicName}"
     $nic = Get-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $rgName
+
+
+    Write-Verbose "Associate Network Security Group (NSG): {$nsgName} to Network Interface (NIC): {$nicName}"
+    $nic.NetworkSecurityGroup = $nsg
+
+    $nic | Set-AzureRmNetworkInterface
 } 
 else 
 {
