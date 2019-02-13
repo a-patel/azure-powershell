510133b021f518f4d5d2f09acbc0a96ba43f6777
Aashish Patel <toaashishpatel@gmail.com>
2019-02-13 15:55:35 +0530

    Update InstallWebServer.ps1

--- scripts/InstallWebServer.ps1
+++ scripts/InstallWebServer.ps1
@@ -1,1 +1,28 @@
 
+
+<# Install web server #>
+
+
+Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -ComputerName Server1 -WhatIf
+
+Install-WindowsFeature -name Web-Server -IncludeManagementTools
+
+
+
+<#
+
+
+
+#>
+
+
+
+
+
+<#
+
+https://docs.microsoft.com/en-us/powershell/module/servermanager/install-windowsfeature?view=winserver2012r2-ps&viewFallbackFrom=win10-ps
+https://www.solvps.com/blog/installing-iis-powershell-windows-vps/
+
+#>
+
